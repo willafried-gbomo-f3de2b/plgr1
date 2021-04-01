850b5019f2fa351f69a39c47e64d097127bafb59
x <x@x.com>
2021-04-01 17:44:12 +0900

    a

--- lot1/main.cpp
+++ lot1/main.cpp
@@ -1,16 +1,32 @@
 #include "sep.h"
 
+#include <windows.h>
+
 #include <iostream>
 #include <string>
 
-
-using std::cout, std::wcout, std::endl;
+using std::cout, std::wcout, std::endl, std::hex;
 
 int main(void)
 {
     cout << "main." << endl;
 
     f1();
+
+    LCID lcid = ::GetThreadLocale();
+    cout << "GetThreadLocale, LCID: 0x" << hex << lcid << endl;
+
+    char *l1;
+    // l1 = setlocale(LC_ALL, "English_United States.utf8");
+    l1 = setlocale(LC_ALL, nullptr);
+    cout << "setlocale(), -> " << (l1 ? l1 : "?") << endl;
+
+    lcid = ::GetThreadLocale();
+    cout << "GetThreadLocale, LCID: 0x" << hex << lcid << endl;
+
+    l1 = setlocale(LC_ALL, "");
+    cout << "setlocale(), -> " << (l1 ? l1 : "?") << endl;
+
 
     cout << "end." << endl;
 }
