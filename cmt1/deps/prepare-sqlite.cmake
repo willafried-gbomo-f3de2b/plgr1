2e6957deb6c6debc492ebb4087b34fd99f0a1056
a <a@a.com>
2021-04-06 23:24:14 +0900

    a

--- cmt1/deps/prepare-sqlite.cmake
+++ cmt1/deps/prepare-sqlite.cmake
@@ -1,15 +1,17 @@
 # thirdparty/sqlite
 
 message("  >>> downloading sqlite... ")
-file(DOWNLOAD https://www.sqlite.org/2021/sqlite-amalgamation-3350400.zip sqlite-download.zip)
+file(DOWNLOAD https://www.sqlite.org/2021/sqlite-amalgamation-3350400.zip ${THIRDPARTY}/sqlite-download.zip)
+
+file(REMOVE_RECURSE ${OUT} ${THIRDPARTY}/sqlite-amalgamation)
 
 message("  >>> extracting sqlite... ")
 file(ARCHIVE_EXTRACT 
-	INPUT sqlite-download.zip
-	DESTINATION .
+	INPUT ${THIRDPARTY}/sqlite-download.zip
+	DESTINATION ${THIRDPARTY}
 	)
 
-file(GLOB OUT sqlite-amalgamation-*)
+file(GLOB OUT ${THIRDPARTY}/sqlite-amalgamation-*)
 message("  >>> renaming sqlite directory... [sqlite-amalgamation]")
-file(RENAME ${OUT} sqlite-amalgamation)
+file(RENAME ${OUT} ${THIRDPARTY}/sqlite-amalgamation)
 
