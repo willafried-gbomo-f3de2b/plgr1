3985fe57b90bbf9fd1ae7df5f39abe52c244f9b5
x <x@x.com>
2021-04-06 10:36:01 +0900

    a

--- cmt1/deps/fetch-deps.cmake
+++ cmt1/deps/fetch-deps.cmake
@@ -1,4 +1,4 @@
 # thirdparty/sqlite
 
-message(" >> downloading sqlite... ")
+message("  >>> downloading sqlite... ")
 file(DOWNLOAD https://www.sqlite.org/src/zip/sqlite.zip ${CMAKE_CURRENT_SOURCE_DIR}/sqlite.zip)
