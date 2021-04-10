79f76e5e39914a4ca4baf729ef0fc61987c15919
a <a@a.com>
2021-04-11 00:32:03 +0900

    a

--- cmt1/deps/install-libebml.cmake
+++
@@ -1,6 +0,0 @@
-
-add_custom_command(TARGET deps-libebml
-    COMMAND ${CMAKE_COMMAND} -E echo " >> ${BASENAME} install"
-    COMMAND ${CMAKE_COMMAND} --install . --prefix ${BLDDIR}/out --config $<CONFIG>
-    WORKING_DIRECTORY ${BLDDIR}/deps-libebml
-    )
--- cmt1/deps/install-libmatroska.cmake
+++
@@ -1,6 +0,0 @@
-
-add_custom_command(TARGET deps-libmatroska
-    COMMAND ${CMAKE_COMMAND} -E echo " >> ${BASENAME} install"
-    COMMAND ${CMAKE_COMMAND} --install . --prefix ${BLDDIR}/out --config $<CONFIG>
-    WORKING_DIRECTORY ${BLDDIR}/deps-libmatroska
-    )
