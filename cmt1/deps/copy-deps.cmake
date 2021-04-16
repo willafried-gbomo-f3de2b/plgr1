13afa44356d838cc44d8ef3e83163db2eabf75ba
x <x@x.com>
2021-04-16 09:10:33 +0900

    a

---
+++ cmt1/deps/copy-deps.cmake
@@ -0,0 +1,28 @@
+# dep/*
+
+# args: CPYDIR, DSTDIR, CONFIG
+
+if (CPYDIR)
+    #message("copying dep files...")
+    file(MAKE_DIRECTORY ${CPYDIR}/${CONFIG})
+
+    file(MAKE_DIRECTORY ${CPYDIR}/${CONFIG}/lib)
+    file(GLOB_RECURSE SUB "${DSTDIR}/*.lib")
+    foreach(ITEM ${SUB})
+        file(RELATIVE_PATH REL_ITEM "${DSTDIR}" "${ITEM}")
+        message(" ${REL_ITEM}")
+        file(COPY ${ITEM} DESTINATION ${CPYDIR}/${CONFIG}/lib)
+    endforeach()
+
+    file(MAKE_DIRECTORY ${CPYDIR}/${CONFIG}/bin)
+    file(GLOB_RECURSE SUB "${DSTDIR}/*.dll")
+    foreach(ITEM ${SUB})
+        file(RELATIVE_PATH REL_ITEM "${DSTDIR}" "${ITEM}")
+        message(" ${REL_ITEM}")
+        file(COPY ${ITEM} DESTINATION ${CPYDIR}/${CONFIG}/lib)
+    endforeach()
+
+    message(" coping includes...")
+    file(COPY "${DSTDIR}/include" DESTINATION "${CPYDIR}/${CONFIG}")
+else()
+endif()
