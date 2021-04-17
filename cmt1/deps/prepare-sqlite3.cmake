2c63d3815eb8374c7f9843a37ad4bd7b49e859e8
c-on-vaio <c@vaio>
2021-04-18 03:22:09 +0900

    a

--- cmt1/deps/prepare-sqlite3.cmake
+++ cmt1/deps/prepare-sqlite3.cmake
@@ -3,29 +3,32 @@
 # sqlite3 amalgamation zip file
 set(SQLITE3_AMALGAMATION_ZIP_DOWNLOAD_URL "https://www.sqlite.org/2021/sqlite-amalgamation-3350400.zip" CACHE STRING "")
 
+# make directory
+file(MAKE_DIRECTORY ${SRCDIR}/sqlite3)
+
 # download
 message("  >> downloading ${SQLITE3_AMALGAMATION_ZIP_DOWNLOAD_URL} ...")
-file(DOWNLOAD ${SQLITE3_AMALGAMATION_ZIP_DOWNLOAD_URL} ${SRCDIR}/sqlite-amalgamation.zip)
+file(DOWNLOAD ${SQLITE3_AMALGAMATION_ZIP_DOWNLOAD_URL} ${SRCDIR}/sqlite3/sqlite-amalgamation.zip)
 
 # extract
 message("  >> extracting...")
-file(ARCHIVE_EXTRACT INPUT ${SRCDIR}/sqlite-amalgamation.zip
-	DESTINATION ${SRCDIR}
+file(ARCHIVE_EXTRACT INPUT ${SRCDIR}/sqlite3/sqlite-amalgamation.zip
+	DESTINATION ${SRCDIR}/sqlite3
 )
 
 # rename
-file(GLOB LST "${SRCDIR}/sqlite-amalgamation-*")
+file(GLOB LST "${SRCDIR}/sqlite3/sqlite-amalgamation-*")
 foreach(ITEM ${LST})
 	if(IS_DIRECTORY ${ITEM})
 		#message("ITEM: ${ITEM}")
 		#cmake_path(GET ${ITEM} PARENT_PATH DIR)
 		#cmake_path(GET ${ITEM} FILE_NAME FN)
 		#message("  >> renaming ${FN} -> sqlite-amalgamation")
-		file(RENAME ${ITEM} ${SRCDIR}/sqlite-amalgamation)
+		file(RENAME ${ITEM} ${SRCDIR}/sqlite3/sqlite-amalgamation)
 	endif()
 endforeach()
 
 # copy cmake file
 message("  >> copy & rename cmake file ...")
-file(COPY ${CMAKE_CURRENT_SOURCE_DIR}/sqlite3.cmake DESTINATION ${SRCDIR}/sqlite-amalgamation)
-file(RENAME ${SRCDIR}/sqlite-amalgamation/sqlite3.cmake ${SRCDIR}/sqlite-amalgamation/CMakeLists.txt)
+file(COPY ${CMAKE_CURRENT_SOURCE_DIR}/sqlite3.cmake.in DESTINATION ${SRCDIR}/sqlite3)
+file(RENAME ${SRCDIR}/sqlite3/sqlite3.cmake.in ${SRCDIR}/sqlite3/CMakeLists.txt)
