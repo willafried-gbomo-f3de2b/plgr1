c49da5a504f1925be3723a78ff1d0d86d310800e
c-on-vaio <c@vaio>
2021-04-18 14:45:25 +0900

    a

---
+++ cmt1/deps/prepare-pthreads4w.cmake
@@ -0,0 +1,6 @@
+# thirdparty/pthreads4w
+
+execute_process(COMMAND git clone https://github.com/Vollstrecker/pthreads4w.git
+	WORKING_DIRECTORY ${SRCDIR}
+	)
+
---
+++ cmt1/deps/prepare-pupnp.cmake
@@ -0,0 +1,6 @@
+# thirdparty/pupnp
+
+execute_process(COMMAND git clone https://github.com/pupnp/pupnp.git
+	WORKING_DIRECTORY ${SRCDIR}
+	)
+
