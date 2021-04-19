35aa5320ea13c5c561b6c8660d442f216141c693
x <x@x.com>
2021-04-19 11:08:18 +0900

    a

--- cmt1/deps/clean-deps.cmake
+++ cmt1/deps/clean-deps.cmake
@@ -7,7 +7,7 @@ set(TARGETNAME "clean-deps")
 add_custom_target(${TARGETNAME})
 
 add_custom_command(TARGET ${TARGETNAME}
-    COMMAND ${CMAKE_COMMAND} -E cleaning deps...
+    COMMAND ${CMAKE_COMMAND} -E echo "cleaning deps..."
 	COMMAND ${CMAKE_COMMAND} -E remove_directory ${DSTDIR} 
 )
 
