7685f811f7f4b3e6e5bd066a2c0da733d4dae5a2
xv@xv.com <xv@xv.com>
2021-04-19 12:12:33 +0900

    a

--- cmt1/deps/depend-pupnp.cmake
+++ cmt1/deps/depend-pupnp.cmake
@@ -4,8 +4,8 @@ add_dependencies(deps deps-pupnp)
 
 if(MSVC)
     add_dependencies(deps-pupnp deps-pthreads4w)
-    add_dependencies(deps-pupnp clean-deps)
 endif()
 
+add_dependencies(deps-pupnp clean-deps)
 
 
