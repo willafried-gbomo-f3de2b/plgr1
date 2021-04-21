870ccf41c5b3e8afe8ff384822337e370c35ea7a
x <x@x.com>
2021-04-21 12:22:01 +0900

    a

--- cmt1/deps/prepare-pupnp.cmake
+++ cmt1/deps/prepare-pupnp.cmake
@@ -2,5 +2,9 @@
 
 execute_process(COMMAND git clone https://github.com/pupnp/pupnp.git
 	WORKING_DIRECTORY ${SRCDIR}
-	)
+)
+
+execute_process(COMMAND git checkout 332fa8879e457487fc3c8800c4461d74fcb29527
+	WORKING_DIRECTORY ${SRCDIR}/pupnp
+)
 
