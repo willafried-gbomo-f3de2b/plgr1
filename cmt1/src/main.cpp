b8f50e80ae68596f42119e4eddadb552bff4334b
x <x@x.com>
2021-04-23 11:27:10 +0900

    a

--- cmt1/src/main.cpp
+++ cmt1/src/main.cpp
@@ -4,22 +4,30 @@
 #include <iostream>
 #include <string>
 
-
 using std::cout, std::endl;
 
 int main(void)
 {
     cout << "main()." << endl;
+
+    const char *locstr = setlocale(LC_ALL, "");
+    cout << "locale: " << locstr << endl;
+
     matroska_init();
 
     sqlite3_initialize();
 
-    UpnpLib* upnplib = nullptr;
     std::string ifname = "VPN 第二";
     std::string logname = "pupnp1.log";
+
+    UpnpLog *upnplog = UpnpLog_new();
+    UpnpSetLogLevel(upnplog, UPNP_INFO);
+    UpnpLib *upnplib = nullptr;
     UpnpInit2(&upnplib, ifname.c_str(), 0, logname.c_str());
 
     auto fsd = FLAC__stream_decoder_new();
-    
-}
 
+    UpnpFinish(upnplib);
+
+    cout << "main(), end." << endl;
+}
