c046f98a375418b99c3e7961d8975675c770f274
x <x@x.com>
2021-04-26 14:44:00 +0900

    a

--- cmt1/src/main.cpp
+++ cmt1/src/main.cpp
@@ -27,21 +27,7 @@ int main(void)
     std::string ifname;
     int port = 0;
     std::string logname = "pupnp1.log";
-    // std::ifstream fs;
-    // fs.open("./nw-name.txt", std::ios::in | std::ios::binary);
-    // if (!fs.is_open())
-    // {
-    //     //cout << "err: cannot open file ./nm-name.txt" << endl;
-    //     cout << "file not found: ./nm-name.txt" << endl;
-    // }
-    // else
-    // {
-    //     std::getline(fs, ifname);
-    //     ifname.erase(std::remove(std::begin(ifname),
-    //         std::end(ifname), '\r'),
-    //         std::end(ifname));
-    //     fs >> port;
-    // }
+    
     ifname = cfg.values.nw_name;
     port = cfg.values.nw_port;
 
