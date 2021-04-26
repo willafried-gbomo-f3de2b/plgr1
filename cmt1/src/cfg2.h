9444a85f11c5d98997a1d0bfc53a745c246f7d12
cov <c@ov>
2021-04-27 02:18:01 +0900

    a

--- cmt1/src/cfg2.h
+++ cmt1/src/cfg2.h
@@ -48,26 +48,35 @@ namespace Cfg2::detail
 			using std::cout, std::endl;
 
 			std::vector<T> buf(10 * 1024);
-
+			int num_lines = 0;
 			while (!m_strm.eof())
 			{
+				num_lines++;
+
 				// skip first spaces
-				std::ws(m_strm);
+				//std::ws(m_strm);
 
 				// read a line
 				auto len = m_strm.getline(buf.data(), buf.size())
 							   .gcount();
-				std::basic_string_view<T> sv(buf.data());
+				std::basic_string_view<T> line(buf.data());
 
-				// remove CR, LF, if any
-				while (sv.size() &&
-					   (sv.back() == '\r' || sv.back() == '\n'))
-					sv.remove_suffix(1);
+				// // remove CR, LF, if any
+				// while (line.size() &&
+				// 	   (line.back() == '\r' || line.back() == '\n'))
+				// 	line.remove_suffix(1);
 
 				// remove trailing spaces, if any
-				while (sv.size() && std::isspace(sv.back(), std::locale #()))
-					sv.remove_suffix(1);
-				cout << "sv (" << sv.size() << "): " << sv << "|" << endl;
+				while (line.size() && std::isspace(line.back(), std::locale()))
+					line.remove_suffix(1);
+
+				// remove preceding spaces, if any
+				while (line.size() && std::isspace(line.front(), std::locale()))
+					line.remove_prefix(1);
+
+				if (!line.size())
+					continue;
+				cout << "line:" << num_lines << "(" << line.size() << "): " << line << "|" << endl;
 			}
 
 			return true;
