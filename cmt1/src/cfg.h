4aed712868181a8d20fcc6ebda3697ebf8a85ff6
cov <c@ov>
2021-04-29 04:02:33 +0900

    a

--- cmt1/src/cfg.h
+++ cmt1/src/cfg.h
@@ -1,20 +1,155 @@
 #pragma once
 
-#include <string>
+#include <fstream>
+#include <iostream>
 #include <istream>
-#include <map>
+#include <locale>
+#include <string_view>
+#include <vector>
 
-struct Cfg
+namespace Cfg {
+
+namespace detail {
+template <class> struct CfgReader;
+} // namespace detail
+
+template <class T> bool ReadCfg(std::basic_istream<T>& strm)
 {
-    struct Values
-    {
-        std::string nw_name;
-        int nw_port;
-        std::string web_root;
-    } values;
+	detail::CfgReader<T> cfg(strm);
+	return cfg.Read();
+}
 
-    std::multimap<std::string, std::string> contents;
+template <class T> bool ReadCfg(const T* path)
+{
+	std::basic_ifstream<T> ifs;
+	ifs.open(path);
+	if (!ifs.is_open())
+		return false;
+	return ReadCfg<T>(ifs);
+}
+
+} // namespace Cfg
+
+namespace Cfg::detail {
+
+template <class T> struct CfgReader 
+{
+	typedef std::basic_string<T> str_t;
+	typedef std::basic_string_view<T> sv_t;
+
+	std::basic_istream<T>& m_strm;
+
+	CfgReader(std::basic_istream<T>& s) : m_strm(s) {}
+
+	bool Read(void)
+	{
+		using std::cout, std::endl;
+
+		std::vector<T> buf(10 * 1024);
+		int num_lines = 0;
+		while (!m_strm.eof()) {
+			num_lines++;
+
+			// read a line
+			auto len = m_strm.getline(buf.data(), buf.size()).gcount();
+			sv_t line(buf.data());
+
+			// remove trailing spaces, if any
+			while (line.size() && std::isspace(line.back(), std::locale()))
+				line.remove_suffix(1);
+
+			// remove preceding spaces, if any
+			while (line.size() && std::isspace(line.front(), std::locale()))
+				line.remove_prefix(1);
+
+			if (!line.size())
+				continue;
+			cout << "line:" << num_lines << "(" << line.size() << "): "
+				<< line << "|" << endl;
+
+			str_t key, val;
+			ParseLine(line, key, val);
+
+			if (key.size()) {
+				cout << "  key:" << key << "|";
+			}
+			cout << "  val:" << val << "|" << endl;
+		}
+
+		return true;
+	}
+
+	void ParseLine(const sv_t& line, str_t& out_key, str_t& out_val)
+	{
+		typename sv_t::const_iterator sep, end_pos;
+		sep = end_pos = line.cend();
+		T quote = 0;
+		unsigned int escape = 0;
+
+		for (auto iter = line.cbegin(); 
+			iter != line.cend() && end_pos == line.cend();
+			iter++, escape >>= 1)
+		{
+			if (escape != 0)
+				continue;
+
+			if (quote == 0) {
+				switch (*iter) {
+				case '"': case '\'':
+					quote = *iter;
+					break;
+				case '=':
+					sep = iter;
+					break;
+				case '#':
+					end_pos = iter;
+					break;
+				}
+			}
+			else {
+				switch (*iter) {
+				case '"': case '\'':
+					if (*iter == quote) {
+						quote = 0;
+					}
+					break;
+				}
+			}
+
+			if (*iter == '\\') {
+				escape = 2;
+			}
+		}
+
+		sv_t val;
+		if (sep != line.cend()) {
+			sv_t key(&line.front(), sep - line.cbegin());
+			out_key = SvTrimSpaces(key);
+
+			if (++sep != line.cend()) {
+				sv_t val(&*sep, end_pos - sep);
+				val = out_val = SvTrimSpaces(val);
+			}
+		}
+		else {
+			out_key.clear();
+
+			if (line.size()) {
+				sv_t val(&line.front(), end_pos - line.cbegin());
+				out_val = SvTrimSpaces(val);
+			}
+		}
+	}
+
+	sv_t& SvTrimSpaces(sv_t& sv)
+	{
+		while (sv.size() && isspace(sv.front()))
+			sv.remove_prefix(1);
+		while (sv.size() && isspace(sv.back()))
+			sv.remove_suffix(1);
+		return sv;
+	}
 };
 
-bool ReadCfg(const char *cfg_file_path, Cfg &out_cfg);
-bool ReadCfg(std::istream &strm, Cfg &out_cfg);
+
+} //namespace Cfg2::detail
--- cmt1/src/main.cpp
+++ cmt1/src/main.cpp
@@ -2,7 +2,6 @@
 #include "deps.h"
 
 #include "cfg.h"
-#include "cfg2.h"
 
 #include <iostream>
 #include <fstream>
@@ -17,10 +16,10 @@ int main(void)
 	const char* locstr = setlocale(LC_ALL, "");
 	cout << "locale: " << locstr << endl;
 
-	Cfg cfg = {};
+	//Cfg cfg = {};
 
 	//bool b = ReadCfg("cmt1.cfg", cfg);
-	bool b = Cfg2::ReadCfg("cmt1.cfg");
+	bool b = Cfg::ReadCfg("cmt1.cfg");
 
 	matroska_init();
 
@@ -30,8 +29,8 @@ int main(void)
 	int port = 0;
 	std::string logname = "pupnp1.log";
 
-	ifname = cfg.values.nw_name;
-	port = cfg.values.nw_port;
+	// ifname = cfg.values.nw_name;
+	// port = cfg.values.nw_port;
 
 	UpnpLog* upnplog = UpnpLog_new();
 	UpnpInitLog(upnplog);
