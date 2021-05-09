0617c7293485bc24d0a8a35768c09d0d6e5e41d7
r7 <r7.com>
2021-05-09 14:26:04 +0900

    a

--- cmt1/src/path.h
+++ cmt1/src/path.h
@@ -5,7 +5,6 @@
 
 #ifdef WIN32
 #include <Windows.h>
-#include <Psapi.h>
 #else //#ifdef WIN32
 #endif //#ifdef WIN32
 
@@ -65,6 +64,7 @@ std::basic_string<charT> GetSelfBaseNameTmpl(void)
 	}
 	return name.substr(0, pos);
 }
+
 
 template <class charT>
 std::basic_string<charT> GetSelfExtTmpl(void)
@@ -136,9 +136,68 @@ inline std::wstring GetSelfExt(void)
 
 #else //#ifdef WIN32
 
-inline std::string GetExeDir(void)
+template <class charT>
+std::basic_string<charT> GetSelfPathTmpl(void)
 {
+	typedef std::basic_string<charT> str_t;
+	std::vector<charT> buf(1000);
+	DWORD n = ::GetModuleFileName(NULL, buf.data(), (DWORD)buf.size());
+	if (n == 0 || n == (DWORD)buf.size()) {
+		return "";
+	}
+	return std::basic_string<charT>(&buf.front(), n);
+}
 
+
+template <class charT>
+std::basic_string<charT> GetSelfDirTmpl(void)
+{
+	typedef std::basic_string<charT> str_t;
+	str_t path = GetSelfPathTmpl<charT>();
+	auto pos = path.find_last_of('\\');
+	if (pos == str_t::npos) {
+		return std::move(path);
+	}
+	return path.substr(0, pos);
+}
+
+
+template <class charT>
+std::basic_string<charT> GetSelfNameTmpl(void)
+{
+	typedef std::basic_string<charT> str_t;
+	str_t path =  GetSelfPathTmpl<charT>();
+	auto pos = path.find_last_of('\\');
+	if (pos == str_t::npos) {
+		return std::move(path);
+	}
+	return path.substr(pos + 1);
+}
+
+
+template <class charT>
+std::basic_string<charT> GetSelfBaseNameTmpl(void)
+{
+	typedef std::basic_string<charT> str_t;
+	str_t name =  GetSelfNameTmpl<charT>();
+	auto pos = name.find_last_of('.');
+	if (pos == str_t::npos) {
+		return std::move(name);
+	}
+	return name.substr(0, pos);
+}
+
+
+template <class charT>
+std::basic_string<charT> GetSelfExtTmpl(void)
+{
+	typedef std::basic_string<charT> str_t;
+	str_t name =  GetSelfNameTmpl<charT>();
+	auto pos = name.find_last_of('.');
+	if (pos == str_t::npos) {
+		return std::move(name);
+	}
+	return name.substr(pos + 1);
 }
 
 #endif //#ifdef WIN32
